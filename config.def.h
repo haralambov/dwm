c0c59d5bfce0fa9bb3fb3842746f75778e796f2f
Zlatomir Haralambov <z.haralambov@gmail.com>
2021-11-21 22:09:42 +0200

    Comments out the firefox rule

--- config.def.h
+++ config.def.h
@@ -38,7 +38,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	// { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
