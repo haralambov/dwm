0f2acea794b588c7cf629a23eeba56e520bad3ab
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-01-19 20:17:18 +0200

    Changes window border color to gruvbox blue

--- config.def.h
+++ config.def.h
@@ -24,11 +24,11 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char col_red[]         = "#cc241d";
+static const char col_blue[]        = "#458588";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_red  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_blue  },
 };
 
 /* tagging */
