90821bb58281f5f778a9001d3a4eea8ebba95ecc
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-01-30 11:58:42 +0200

    Adds qbittorrent and mpv rules

--- config.def.h
+++ config.def.h
@@ -44,11 +44,13 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ "feh",     NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	/* class         instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Firefox",     NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "qBittorrent", NULL,     NULL,           1 << 6,    0,          0,          -1,        -1 },
+	{ "mpv",         NULL,     NULL,           1 << 5,    0,          0,          -1,        -1 },
+	{ "St",          NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "feh",         NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ NULL,          NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
